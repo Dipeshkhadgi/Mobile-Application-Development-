4905fcf1f7b6918becf89438cefed7ecaee2fc39
dipesh kumar khadgi <d8khadgi@gmail.com>
2024-11-20 00:58:13 +0545

    question7

---
+++ lib/question7.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter the first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Choose the operation (+, -, *, /): ");
+  String operation = stdin.readLineSync()!;
+
+  double result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      print("Result: $num1 + $num2 = $result");
+      break;
+    case '-':
+      result = num1 - num2;
+      print("Result: $num1 - $num2 = $result");
+      break;
+    case '*':
+      result = num1 * num2;
+      print("Result: $num1 * $num2 = $result");
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+        print("Result: $num1 / $num2 = $result");
+      } else {
+        print("Error: Division by zero is not allowed.");
+      }
+      break;
+    default:
+      print("Invalid operation. Please choose one of the following: +, -, *, /");
+  }
+}
