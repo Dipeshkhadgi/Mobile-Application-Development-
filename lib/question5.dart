52a1a63e50952549751f68cd043e904c56f0e705
dipesh kumar khadgi <d8khadgi@gmail.com>
2024-11-20 00:53:13 +0545

    question 5

---
+++ lib/question5.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a positive number: ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  if (n > 0) {
+    int sum = (n * (n + 1)) ~/ 2; // Using the formula for the sum of the first n natural numbers
+    print("The sum of the first $n natural numbers is $sum.");
+  } else {
+    print("Please enter a positive number.");
+  }
+}
